984d3f91466ac7af7ffff26fc31f56e5645d39b5
flanderz91 <flanderz91@hotmail.com>
2016-01-29 19:36:03 -0300

    con linea de tiempo version 1

--- static/js/timelineSetup.js
+++ static/js/timelineSetup.js
@@ -11,35 +11,56 @@ var lngDimension;
 var idDimension;
 var idGrouping;
 
+var fechaIni;//fecha de inicio del grafico
+var fechaFin;//fecha de termino del grafico
+
+var ini;
+var fin;
+
+var width = 700;
+var height = 400;
+var padding = 100;
+var format = d3.time.format("%Y-%m-%d");
+
+
 function init() {
   initCrossfilter();
+
+  idDimension = filter.dimension(function(p, i) { return i; });
+  idGrouping = idDimension.group(function(id) { return id; });
+
   renderAll();
 }
 
+function getDates(){
+  indexFinalDate=newsMap.length-1;
+  fechaIni=format.parse(newsMap[0]._source.date.substr(0,10));
+  fechaFin=format.parse(newsMap[indexFinalDate]._source.date.substr(0,10));
+
+}
 
 function initCrossfilter() {
-  filter = crossfilter(points);
+  filter = crossfilter(newsMap);
 
   // simple dimensions and groupings for major variables
-  val2Dimension = filter.dimension(
-      function(p) {
-        return p.val2;
+  dateDimension = filter.dimension(
+      function(p) {return format.parse(p._source.date.substr(0,10));
       });
-  val2Grouping = val2Dimension.group(
+  dateDimensionGrouping = dateDimension.group(
       function(v) {
-        return Math.floor(v / 25) * 25;
+        return v;
       });
 
+  getDates();
   // initialize charts (helper function in chart.js)
   // taken directly from crossfilter's example
   charts = [
     barChart()
-      .dimension(val2Dimension)
-      .group(val2Grouping)
+      .dimension(dateDimension)
+      .group(dateDimensionGrouping)
       .x(d3.scale.linear()
-          .domain([0, 1000])
-          .rangeRound([0, 40 * 10]))
-      .filter([75, 525])
+          .domain([fechaFin,fechaIni])
+          .rangeRound([padding, width - padding * 2]))
   ];
 
   // bind charts to dom
@@ -58,9 +79,26 @@ function updateCharts() {
   domCharts.each(render);
 }
 
+function updateMarkers() {
+  markers.clearLayers();//borro todo del mapa
+  var pointIds = idGrouping.all();//obtengo ids y values(1 o 0) para ver cual mostrar o no
+  for(var i=0;i<newsMap.length;i++){
+    var pointId = pointIds[i];
+    //console.log(pointId);
+    if(pointId.value > 0){
+      var latLong=getLatLong(newsMap[i]._source.countrylocation);
+      marker = L.marker(new L.LatLng(latLong[0], latLong[1]));
+      markers.addLayer(marker);
+    }   
+  }
+  map.addLayer(markers);
+}
 
 // Whenever the brush moves, re-render charts and map markers
 function renderAll() {
+  //var pointIds = dateDimensionGrouping.all();
+  //console.log(pointIds);
+  updateMarkers();
   updateCharts();
 }
 
